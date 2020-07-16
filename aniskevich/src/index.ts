30e62aa2d07a79319fa73b23689925a1d4f21c83
Boris Aniskevich <aniskevich.bo@gmail.com>
2020-07-16 20:48:05 +1000

    HW1

--- aniskevich/src/index.ts
+++ aniskevich/src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello world!')
+import { gTTS } from 'gtts.js'
+import * as readline from 'readline'
+import chalk from 'chalk'
+const player = require('play-sound')()
+
+const filePath = 'output.mp3'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('Please enter your text \n', (input: string) => {
+    console.log('Processing, please wait....')
+    const tts = new gTTS(input)
+    rl.close()
+    tts.save(filePath)
+    .then(() => {
+        player.play(filePath, (err: any) => {
+            if (err) throw err
+        })
+        console.log(chalk.red(`Your text is: ${input}`))
+    })
+    .catch((err) => {
+        throw err
+    })
+})
+
