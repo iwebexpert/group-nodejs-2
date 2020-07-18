5d6fc94b59f7dadba3886d42bd2531ba66210c06
Daria Diachkova <daria.diachkova@mail.ru>
2020-07-18 13:02:50 +0300

    Task 2

---
+++ dariadia/headsTailsStat.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+const win = "Won";
+const lose = "Lost";
+
+const loggerFilePath = "headsTailsLogger.txt";
+
+const countConsecutive = (data, flag) => {
+  return Math.max(
+    ...data.reduce(
+      (sequence_lengths, x) => {
+        x === flag ? sequence_lengths[0]++ : sequence_lengths.unshift(0);
+        return sequence_lengths;
+      },
+      [0]
+    )
+  );
+};
+
+const prepareData = (data) => {
+  const dataHelper = data.split(/\n/);
+  const dataHelperArray = dataHelper.slice(0, dataHelper.length - 1); //last element is always an empty new line
+
+  const total = dataHelperArray.length;
+  const gamesWon = dataHelperArray.filter((gameResult) => gameResult === win)
+    .length;
+  const gamesLost = dataHelperArray.filter((gameResult) => gameResult === lose)
+    .length;
+  const winningRate = gamesWon / gamesLost;
+  const winningSpreeCount = countConsecutive(dataHelperArray, win);
+  const losingSpreeCount = countConsecutive(dataHelperArray, lose);
+
+  return `
+    Total: ${total}
+    Games won: ${gamesWon}
+    Games lost: ${gamesLost}
+    Winning rate: ${winningRate}
+    Max. wining spree: ${winningSpreeCount}
+    Max. losing spree: ${losingSpreeCount}
+    `;
+};
+
+const readStat = (filePath) => {
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) throw err;
+
+    console.log(prepareData(data));
+  });
+};
+
+readStat(loggerFilePath);
