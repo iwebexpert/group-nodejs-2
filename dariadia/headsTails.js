098395ee436d40642c03b15b9b2c0a79f675afcf
Daria Diachkova <daria.diachkova@mail.ru>
2020-07-18 13:02:59 +0300

    Refactor with variables

--- dariadia/headsTails.js
+++ dariadia/headsTails.js
@@ -3,26 +3,28 @@ const fs = require("fs");
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-let tossCoin = (userChoice) => {
-  let outcome = Math.random() > 0.5 ? "tails" : "heads";
+const win = "Won";
+const lose = "Lost";
+const heads = "heads";
+const tails = "tails";
+const ifUserWon = "You won the toss!";
+const ifUserLost = "You lost the toss.";
 
-  console.log(
-    outcome === userChoice ? "You won the toss!" : "You lost the toss."
-  );
+let tossCoin = (userChoice) => {
+  let outcome = Math.random() > 0.5 ? tails : heads;
+
+  console.log(outcome === userChoice ? ifUserWon : ifUserLost);
 
   fs.appendFile(
     "headsTailsLogger.txt",
-    `${outcome === userChoice ? "Won" : "Lost"}\n`,
-    function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(data);
+    `${outcome === userChoice ? win : lose}\n`,
+    function (err) {
+      if (err) return console.log(err);
     }
   );
 };
 
-rl.question("Type heads or tails, then press ENTER\n", (answer) => {
+rl.question(`Type ${tails} or ${heads}, then press ENTER\n`, (answer) => {
   tossCoin(answer);
   rl.close();
 });
