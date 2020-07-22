6ee74d37724eb3bee41664c42e4a2758df105d77
sinstranger <NZH8dWfFNvpD#>
2020-07-22 10:00:54 +0300

    Correct PullRequest 21

--- Marat Barashkin/index.js
+++ Marat Barashkin/index.js
@@ -1,3 +1,6 @@
 // const lesson1 = require('./lesson_1/index');
 const lesson2 = require('./lesson_2/index');
-lesson2.play();
+const showLogs = require('./lesson_2/showLog');
+// lesson2.gameUserInterface();
+// node index.js ./lesson_2/logs/gamelog.json посмотреть лог через ввод аргумента
+!process.argv[2] ? lesson2.gameUserInterface() : showLogs(process.argv[2]);
