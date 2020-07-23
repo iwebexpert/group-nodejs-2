ac045a026c82fd5146afd0bffb45da9713333213
Kholmanov Andrei Work <ankholmanov@yandex.ru>
2020-07-23 16:35:03 +0700

    lesson2: task2

--- kholmanov_andrey/headAndTail.js
+++ kholmanov_andrey/headAndTail.js
@@ -10,7 +10,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
-console.log(`Введите Орел(1) или Решка(2)`)
+console.log(`Введите Орел(1) или Решка(2) (статистика игр - status)`)
 
 rl.on('line', (cmd) => {
     const number = Math.random() < .5 ? 1 : 2;
@@ -18,6 +18,13 @@ rl.on('line', (cmd) => {
 
     if(cmd === 'exit'){
         rl.close()
+        return false;
+    }
+
+    if(cmd === 'status'){
+        readLog()
+        rl.close()
+        return false;
     }
 
     if (+cmd === number) {
@@ -46,21 +53,71 @@ function writeLog(status) {
         try {
             stats = JSON.parse(data)
 
+            stats.games = stats.games + 1
             if (status === 1) {
                 stats.winner = stats.winner + 1
             } else {
                 stats.loser = stats.loser + 1
             }
-
+            stats.prev.push(status)
         } catch (e) {
             stats = {
+                games: 1,
                 winner: status ? 1 : 0,
-                loser: !status ? 1 : 0
+                loser: !status ? 1 : 0,
+                prev: [
+                    status
+                ]
             }
         }
 
         fs.writeFile(file, JSON.stringify(stats), (err) => {
             if (err) throw err
         })
+    })
+}
+
+function readLog() {
+    const file = 'log.json'
+
+    fs.readFile(file, (err, data) => {
+        if (err) throw err
+
+        stats = JSON.parse(data)
+
+        const result = {
+            count: 0,
+            win: 0,
+            los: 0
+        }
+
+        stats.prev.forEach((item, i, arr) => {
+            if (i > 0 && item !== arr[i - 1]) {
+                if (!item) {
+                    result.win = Math.max(result.count, result.win)
+                } else {
+                    result.los = Math.max(result.count, result.los)
+                }
+                result.count = 0
+            }
+
+            result.count++
+
+            if (i === arr.length - 1) {
+                if (item) {
+                    result.win = Math.max(result.count, result.win)
+                } else {
+                    result.los = Math.max(result.count, result.los)
+                }
+            }
+        })
+
+        // console.log(stats, result)
+        console.log('Количество игр: ', stats.games)
+        console.log('Количество побед: ', stats.winner)
+        console.log('Количество поражений: ', stats.loser)
+        console.log('Соотношение победы/поражения: ', stats.winner/stats.loser)
+        console.log('Побед подряд: ', result.win)
+        console.log('Поражений подряд: ', result.los)
     })
 }
