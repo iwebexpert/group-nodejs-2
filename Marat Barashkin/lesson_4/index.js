e08a80702387d0801ff6b6b42ef45a5c4d036243
sinstranger <sinstranger@yandex.ru>
2020-07-26 21:43:16 +0300

    That I could do when I have time...

---
+++ Marat Barashkin/lesson_4/index.js
@@ -0,0 +1,94 @@
+const { response } = require("express");
+
+/**
+ * Подключаем зависимости
+ */
+const [fs, request, path, express, consolidate, handlebars, cherrio] = [
+	require("fs"),
+	require("request"),
+	require("path"),
+	require("express"),
+	require("consolidate"),
+	require("handlebars"),
+	require("cheerio"),
+];
+const app = express();
+/**
+ * Шаблонизация
+ */
+app.engine("hbs", consolidate.handlebars);
+app.set("view engine", "hbs");
+app.set("views", path.resolve(__dirname, "view"));
+/**
+ * Header и Footer - регистрация частей
+ */
+// TODO: Не нравится - больше похоже на костыль - объясните как сделать лучше - ведь могут быть нужны шаблоны из глубины вложенности view - то есть не из корня
+const HEADER = handlebars.compile(fs.readFileSync(__dirname + "/view/common/header.hbs").toString("utf-8"));
+const FOOTER = handlebars.compile(fs.readFileSync(__dirname + "/view/common/footer.hbs").toString("utf-8"));
+handlebars.registerPartial("header", HEADER);
+handlebars.registerPartial("footer", FOOTER);
+/**
+ * URL и порт сервера
+ */
+const SERVER_URL = "http://localhost";
+const PORT = 8080;
+/**
+ * Middleware для работы с form
+ */
+app.use(express.urlencoded({ extended: false }));
+/**
+ * Middleware для работы JSON
+ */
+app.use(express.json());
+
+/**
+ * Список новостей
+ */
+const NEWS_URL = `https://journal.bookmate.com`;
+
+/**
+ * Главная страница
+ */
+app.get("/", (req, res) => {
+	// TODO: Вопрос: Есть ли в таком методе next и если да, когда он нужен в реальной жизни
+	res.render("mainpage", { documentTitle: `Главная страница`, pagetitle: `Главная` });
+});
+/**
+ * Newsfeed
+ */
+app.get("/newsfeed", (req, res) => {
+	request(NEWS_URL, (err, response, body) => {
+		if (!err && response.statusCode === 200) {
+         const $ = cherrio.load(body);
+         // TODO: Возможно есть более изящный способ собрать newsItems
+			let newsItems = [];
+			$(".pitem").each((index, item) => {
+				newsItems[index] = {
+					srcimage: $(item).find("img").attr("src"),
+					title: $(item).find(".ititle").text(),
+					detailLink: NEWS_URL + $(item).attr("href"),
+				};
+         });
+			res.render("newsfeed", { documentTitle: "Список новостей", pagetitle: "Новости", news: newsItems, });
+		}
+	});
+});
+
+/**
+ * Страница не существует
+ */
+app.use("*", (req, res, next) => {
+	// TODO: Не знаю на сколько уместна обработка ошибок таким образом - научите как правильно - пашет только если разместить внизу всех обработчиков
+	// console.log(`########   Пытаюсь обработать 404  ########`);
+	res.status(404).render("404", {});
+	// console.log(`###########   Обработал   #########`);
+	// TODO: Почему next ошибка
+	// next();
+});
+/**
+ *  Запуск сервера
+ */
+// TODO:  Вопрос: В чем отличие от ```http.createServer(app).listen(80)```
+app.listen(PORT, () => {
+	console.log(`##########   The server was started on: ${SERVER_URL}:${PORT}   ##########`);
+});
