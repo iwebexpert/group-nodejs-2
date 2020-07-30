e56f6b575a0a51efe4528cc067e73cec9efa9bed
cheefs <cheefst@outlook.com>
2020-07-30 09:12:07 +0700

    [practice5] base CRUD для работы с задачами

---
+++ Slyusarenko_Viacheslav/lesson5/models/Task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('Task', taskSchema, 'tasks');
