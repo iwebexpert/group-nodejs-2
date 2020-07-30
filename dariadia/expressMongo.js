8deb025c775e2b486dc36771c19c382bef7ec192
Daria Diachkova <daria.diachkova@mail.ru>
2020-07-30 11:53:17 +0300

    refactor

--- dariadia/expressMongo.js
+++ dariadia/expressMongo.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.render("Hello world!")
+  res.send("Hello world!")
 });
 
 app.get('/tasks', async (req, res) => {
