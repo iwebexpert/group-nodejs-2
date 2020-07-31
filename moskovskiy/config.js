9f5c7447f8f8aee62b8fcd6c4a778f1deacc637c
Maxi-M <maxim.moskovskiy@mail.ru>
2020-07-31 18:35:08 +0300

    Lesson 5 Homework

---
+++ moskovskiy/config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: 'localhost',
+        database: 'node',
+        user: 'node',
+        password: 'node',
+        connectionLimit: 3,
+        waitForConnections: true,
+    }
+}
+module.exports = config;
