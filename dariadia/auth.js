2d96268ddfb148a2a2dbf9b63625c54457fa384d
Daria Diachkova <daria.diachkova@mail.ru>
2020-08-02 13:24:45 +0300

    Add remeber user via cookies

--- dariadia/auth.js
+++ dariadia/auth.js
@@ -1,54 +1,56 @@
-const passport = require('passport')
-const Strategy = require('passport-local').Strategy
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
 
-const UserModel = require('./models/user')
+const UserModel = require("./models/user");
 
 passport.use(
-    new Strategy({usernameField: 'email'}, async (username, password, done) => {
-        const user = await UserModel.findOne({email: username})
+  new Strategy(
+    { usernameField: "email" },
+    async (userEmail, password, done) => {
+      const user = await UserModel.findOne({ email: userEmail });
 
-        if(!user){
-            return done(null, false)
-        }
+      if (!user) {
+        return done(null, false);
+      }
 
-        if(!user.validatePassword(password)){
-            return done(null, false)
-        }
+      if (!user.validatePassword(password)) {
+        return done(null, false);
+      }
 
-        const plainUser = JSON.parse(JSON.stringify(user))
+      const plainUser = JSON.parse(JSON.stringify(user));
 
-        delete plainUser.password
+      delete plainUser.password;
 
-        done(null, plainUser)
-    })
-)
+      done(null, plainUser);
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user._id)
-})
+  done(null, user._id);
+});
 
 passport.deserializeUser(async (id, done) => {
-    const user = await UserModel.findById(id)
-    const plainUser = JSON.parse(JSON.stringify(user))
-    delete plainUser.password
+  const user = await UserModel.findById(id);
+  const plainUser = JSON.parse(JSON.stringify(user));
+  delete plainUser.password;
 
-    done(null, plainUser)
-
-})
+  done(null, plainUser);
+});
 
 module.exports = {
-    initialize: passport.initialize(),
-    session: passport.session(),
-    authenticate: passport.authenticate('local', {
-        successRedirect: '/tasks',
-        failureRedirect: '/auth?error=1',
-    }),
+  initialize: passport.initialize(),
+  session: passport.session(),
+  authenticate: passport.authenticate("local", {
+    successRedirect: "/tasks",
+    failureRedirect: "/auth?error=1",
+  }),
 
-    mustBeAutheticated: (req, res, next) => {
-        if(req.user){
-            next()
-        } else {
-            res.redirect('/auth')
-        }
+  mustBeAutheticated: (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect("/auth");
     }
-}
+  },
+};
