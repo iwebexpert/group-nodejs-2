ccd01589d45e54b9b862825ead2117eb08ae92b3
Maxim Borodin <esau@yandex.ru>
2020-08-07 21:55:45 +0300

    Lesson 7

--- esau/Lesson_5/models/news.js
+++ esau/Lesson_5/models/news.js
@@ -1,7 +1,40 @@
+const config = require('./../config');
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    ...config,
+    connectionLimit: 3,
+    waitForConnections: true
+})
+
 const BaseModel = require('./baseModel');
 
 class News extends BaseModel {
     static tableName = 'news';
+
+    static getById(id)
+    {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, conn) => {
+                if (err) {
+                    reject(err);
+                }
+
+                pool.query('SELECT * FROM ' + this.tableName + ' WHERE ?',
+                    {id},
+                    (err, rawData) => {
+                        if (err) {
+                            reject(err);
+                        }
+
+                        const data = JSON.parse(JSON.stringify(rawData));
+
+                        conn.release();
+                        resolve(data);
+                    })
+            })
+        }));
+    }
 }
 
 module.exports = News;
